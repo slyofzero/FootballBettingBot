a7d8f65e0c0601c963e67ed784cb121a8359d33f
slyofzero <sly.of.zero@gmail.com>
2024-06-26 19:24:17 +0530

    fixes

--- src/bot/commands/start.ts
+++ src/bot/commands/start.ts
@@ -2,8 +2,8 @@ import { BOT_USERNAME } from "@/utils/env";
 import { CommandContext, Context } from "grammy";
 
 export async function startBot(ctx: CommandContext<Context>) {
-  const text = `*Welcome to ${BOT_USERNAME}!!!*\n\n`;
+  const text = `*Welcome to ${BOT_USERNAME}\\!\\!\\!*\n\n`;
   ctx.reply(text, {
-    parse_mode: "MarkdownV2"
+    parse_mode: "MarkdownV2",
   });
 }
